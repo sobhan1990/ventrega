331d6d2f78e2ce1f997a176c508a7c237ec48139
programmerlab <kroy.iips@gmail.com>
2018-11-02 16:51:19 +0530

    sidebar rename

--- Modules/Admin/Resources/views/emails/welcome.blade.php
+++ Modules/Admin/Resources/views/emails/welcome.blade.php
@@ -12,28 +12,15 @@ table {
 </head>
 <body>
  
-<table width="600" border="1" align="center" cellpadding="0" cellspacing="1" bgcolor="#971800" style="background-color:#fff;">
-      <tr>
-          <td align="center" valign="top" bgcolor="#ffffff" >
-            <table width="90%" border="0" cellspacing="0" cellpadding="10" style="margin-bottom:10px;">
-              <tr>
-                <td align="left" valign="top" style="font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000;"> 
-                  <div>
-                        <p>Dear {{$content['first_name']??''}},</p>
-                        <p>Welcome !</p>
-                        <p>
-                          {!!$content['message']??''!!}
-                        </p>
-                      </p>
-                          <p>Best Regards,</p>
-                          <p>{!! $content['greeting']??'' !!}</p>
-
-                  </div>
-                </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
-  </table>
+  <div style="font-family:Arial, Helvetica, sans-serif; font-size:13px; color:#000;">
+        <p>Dear {{$content['first_name']??''}},</p>
+        <p>Welcome !</p>
+        <p>
+          {!!$content['message']??''!!}
+        </p>
+      </p>
+          <p>Best Regards,</p>
+          <p>{!! $content['greeting']??'' !!}</p>
+  </div>
 </body>
 </html>
